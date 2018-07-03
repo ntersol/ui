1ef18db8098e4f220f4db4d4d380f99133996361
Jerrol Krause <jerrolkrause@gmail.com>
2018-07-03 12:00:29 -0700

    Init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
 
   properties: {
     /** Name of application */
-    appName: 'Mello Labs Angular Starter',
+    appName: 'Mello Labs Processor Dashboard',
   },
   settings: {
     /** Is an authentication endpoint available. If so make sure to update the endpoints in this file */
