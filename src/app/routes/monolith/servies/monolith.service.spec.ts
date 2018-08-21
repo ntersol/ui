923e637042d2986261549f6f09dca9e122ebc0a9
Jerrol Krause <jerrolkrause@gmail.com>
2018-08-21 14:44:57 -0700

    Added monolith service scaffolding

---
+++ src/app/routes/monolith/servies/monolith.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { MonolithService } from './monolith.service';
+
+describe('MonolithService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MonolithService]
+    });
+  });
+
+  it('should be created', inject([MonolithService], (service: MonolithService) => {
+    expect(service).toBeTruthy();
+  }));
+});
