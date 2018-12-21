b9c9ba9ccf2a33807aff151324f0cdba87de503a
Jerrol Krause <jerrolkrause@gmail.com>
2018-12-21 11:54:00 -0800

    Better global error handling, error logging

--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -1,39 +1,75 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import { AppSettings } from '../app.settings';
+import { environment } from '$env';
+
+interface AngularError {
+  promise: any;
+  rejection: any;
+  task: any;
+  zone: any;
+  message: string;
+  stack: string;
+  errorMsg?: string;
+  status?: number;
+}
+
+interface LogError {
+  eventTime?: string;
+  level?: string;
+  class: string;
+  message: string;
+  exception: string;
+}
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-  // private errorLogApi = '/api/logerror'; // API to post errors too
-
   constructor(private settings: AppSettings) {}
 
   // Custom error handler for application/angular errors
   // Uses plain JS to eliminate any dependencies that may not be available due to the error
-  public handleError(error: any) {
-    console.error(error);
-    // Only throw application errors to the dom. If the error has an errorMsg then it is an internal error
-    if (this.settings.isBrowser && !error.errorMsg) {
+  public handleError(error: AngularError) {
+    // If is browser
+    // Does not have custom error message
+    // Does not have http status field (to ignore http errors)
+    if (this.settings.isBrowser && !error.errorMsg && !error.hasOwnProperty('status')) {
+      // If error endpoint specified, log errors
+      if (environment.endpoints.errors) {
+        this.logError(error);
+      }
+      this.settings.error$.next(error.message);
+      this.resetState(error);
     }
     // Now throw the error to the console
     throw error;
   } // end handleError
 
   /**
+   * Reset app state
+   * @param error
+   */
+  private resetState(error: AngularError) {
+    console.error({ error: error });
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = '/#/login';
+  }
+
+  /**
    * Log the error to an API
-  private logError(errorMsg: string) {
+   * Use XMLHttpRequest since httpClient may not be available
+   */
+  private logError(error: AngularError) {
     const http = new XMLHttpRequest();
-    const params = 'NEEDKEY=' + errorMsg;
-    http.open('POST', this.errorLogApi, true);
-
-    // Send the proper header information along with the request
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    http.onreadystatechange = () => { // Call a function when the state changes.
-      if (http.readyState === 4 && http.status === 200) {
-        console.warn('Successfully logged error');
-      }
+    const url = environment.endpoints.apiUrl + environment.endpoints.errors;
+    const data: LogError = {
+      level: 'Error',
+      class: '',
+      message: error.message,
+      exception: error.stack,
     };
-    http.send(params);
+    http.open('POST', url, true);
+    // Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+    http.send(JSON.stringify(data));
   } // end logError
-  */
 }
