0e0877e8f0b86098819ac33c30c8b68bf7c61104
Warren Dugan <dugan@warrens-mbp.lan>
2021-03-31 12:15:30 -0700

    Added calendar module and component. Added calendar demo to demo app.

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CalendarModule } from '@ntersol/calendar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CalendarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
