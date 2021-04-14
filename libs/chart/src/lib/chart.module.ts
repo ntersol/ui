055ccfab153a0b08efe01115c2ab21fe5f2552f0
Jerrol Krause <jerrolkrause@gmail.com>
2021-04-14 10:10:40 -0700

    Add charting component

--- libs/chart/src/lib/chart.module.ts
+++ libs/chart/src/lib/chart.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartComponent } from './components/chart.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ChartComponent],
+  exports: [ChartComponent],
 })
-export class ChartModule {}
+export class NtsChartModule {}
