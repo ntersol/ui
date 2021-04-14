b4e3f6c1d956cc159dd8df45057e2dcb52a9ae84
Jerrol Krause <jerrolkrause@gmail.com>
2021-04-14 10:00:43 -0700

    Add visible component

--- libs/visible/src/index.ts
+++ libs/visible/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/visible.module';
+export * from './lib/components';
--- libs/visible/src/lib/visible.module.ts
+++ libs/visible/src/lib/visible.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VisibleComponent } from './components/visible.component';
 
 @NgModule({
+  declarations: [VisibleComponent],
   imports: [CommonModule],
+  exports: [VisibleComponent],
 })
-export class VisibleModule {}
+export class NtsVisibleModule {}
