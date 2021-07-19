72941cfd7a349ddfc2ea458fd9a8de083a08f951
Justin McLellan <justin.mclellan99@gmail.com>
2021-07-19 14:56:09 -0500

    Add ui-state-management documentation

--- apps/showcase/src/app/routes/docs/routes/state-management/routes/ui-store-creator/ui-store-creator.component.ts
+++ apps/showcase/src/app/routes/docs/routes/state-management/routes/ui-store-creator/ui-store-creator.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightService } from '../../../../shared/services/highlight.service';
 import { StateManagementService } from '../../shared/state-management.service';
-// import {  Query } from '@datorama/akita';
+import { install, importExample, usage1, usage2, usage3 } from './code-examples';
+
 @Component({
   selector: 'nts-ui-store-creator',
   templateUrl: './ui-store-creator.component.html',
   styleUrls: ['./ui-store-creator.component.scss'],
 })
 export class UiStoreCreatorComponent implements OnInit {
+  public install = install;
+  public importExample = importExample;
+  public usage1 = usage1;
+  public usage2 = usage2;
+  public usage3 = usage3;
   public name$ = this.sm.uiStore.select$('name');
   public name2$ = this.sm.uiStore.select$(state => state.user?.age);
   public name: string | null = null;
 
-  constructor(private sm: StateManagementService) {}
+  constructor(private sm: StateManagementService, private highlight: HighlightService) { }
 
   ngOnInit(): void {
     this.name$.subscribe(x => console.log(x));
     this.name2$.subscribe(x => console.log(x));
+  }
+
+  ngAfterViewInit() {
+    this.highlight.highlightAll();
   }
 
   public nameSave(name: string | null) {
