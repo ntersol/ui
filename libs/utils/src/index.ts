3742c011f822e700b9c29bbe96b5200405aaf07b
Jerrol Krause <jerrolkrause@gmail.com>
2021-10-05 10:22:44 -0400

    Add image resizer util

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/utils.module';
 export * from './lib/time-ago/time-ago.pipe'
 export * from './lib/script-loader/script-loader.util';
 export * from './lib/formgroup-2-api/formGroup2Api.util';
+export * from './lib/image-resizer/image-resizer.util';
