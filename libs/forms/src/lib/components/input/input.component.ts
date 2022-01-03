f2e36d590f34043508c783ad2c4f6b3b9dd665ea
Jerrol Krause <jerrolkrause@gmail.com>
2022-01-03 12:59:13 -0500

    Add new emitted events, updated blur and focus actions

--- libs/forms/src/lib/components/input/input.component.ts
+++ libs/forms/src/lib/components/input/input.component.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectionStrategy, Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { AbstractControl, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
-// ngModel pass thru
-// https://embed.plnkr.co/nqKUSPWb6w5QXr8a0wEu/?show=preview
+
+@UntilDestroy()
 @Component({
   selector: 'nts-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NtsInputComponent<t> implements OnInit {
+export class NtsInputComponent<t> implements OnInit, OnDestroy {
   /** Standard html placeholder text */
   @Input() placeholder: string | null = null;
   /** Floating label that appears in front of the content and moves above it when focused */
@@ -25,7 +26,6 @@ export class NtsInputComponent<t> implements OnInit {
   @Input() styleClass = '';
   /** Enable/disable this control, uses the form controls status */
   @Input() set disabled(v: boolean | null) {
-    console.log('disabled', v, this.control)
     if (!!this.control && v !== null) {
       !v ? this.control.enable() : this.control.disable();
     }
@@ -51,18 +51,32 @@ export class NtsInputComponent<t> implements OnInit {
   }
   @Input() control?: AbstractControl | null = null;
 
+  /** When the input is focused */
+  @Output() onFocus = new EventEmitter();
+  /** When the input is blurred */
+  @Output() onBlur = new EventEmitter();
+  @Output() onChange = new EventEmitter<t>();
+  /** On keyup event on the input */
+  @Output() onKeyup = new EventEmitter<KeyboardEvent>();
+
   constructor() { }
 
   ngOnInit(): void {
+    // Emit changes to onChange event emitter
+    this.control?.valueChanges.pipe(untilDestroyed(this)).subscribe(v => this.onChange.emit(v))
   }
 
 
-  onBlur() {
+  blur() {
     this.focused = false;
+    this.onBlur.emit();
   }
 
-  onFocus() {
+  focus() {
     this.focused = true;
+    this.onFocus.emit();
   }
+
+  ngOnDestroy(): void { }
 
 }
