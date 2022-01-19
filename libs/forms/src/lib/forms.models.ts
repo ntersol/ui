9ee0992cd4d613e51830ca34eb6e27fcf78f3382
Jerrol Krause <jerrolkrause@gmail.com>
2022-01-19 12:34:50 -0500

    Add new props for support

--- libs/forms/src/lib/forms.models.ts
+++ libs/forms/src/lib/forms.models.ts
@@ -4,15 +4,22 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export module NtsForms {
 
     export interface ValidatorAsyncOptions extends ValidatorOptionsSrc {
+        /** Api to submit request to */
         apiUrl: string | ((control: AbstractControl) => string),
+        /** Httpclient reference */
         httpClient: HttpClient,
+        /** GET or POST request. If post the form control value will be passed as the payload */
         request: 'get' | 'post',
+        /** Map the api response to a format needed by the validator */
         map?: ((apiResponse: any, control: AbstractControl) => ValidationErrors | null | boolean),
+        /** How long to wait between value changes before polling the api */
         debounceTime?: number | null,
+        /** Custom error message, supports a string or a callback function that receives the api response and the form control */
         errorMessage?: string | null | ((apiResponse: any, control: AbstractControl) => string);
     }
 
     export interface ValidatorOptions extends ValidatorOptionsSrc {
+        /** Custom error message, supports a string or a callback function that receives the form control */
         errorMessage?: string | null | ((control: AbstractControl) => string);
     }
 
