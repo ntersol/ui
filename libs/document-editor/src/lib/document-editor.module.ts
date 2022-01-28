8a167759e0e841535102be4a177069c8f04835ff
Warren Dugan <warrendugan@GMAIL.COM>
2022-01-28 10:23:46 -0800

    adds provider for pls in doc editor lib

--- libs/document-editor/src/lib/document-editor.module.ts
+++ libs/document-editor/src/lib/document-editor.module.ts
@@ -1,24 +1,22 @@
+import { CommonModule, PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { TabViewModule } from 'primeng/tabview';
 import { ButtonModule } from 'primeng/button';
-import { ProgressBarModule } from 'primeng/progressbar';
-
-import { EditorComponent } from './components/editor/editor.component';
-import { DocumentComponent } from './components/document/document.component';
-import { DocumentEditorService } from './shared/services/document-editor.service';
-import { ViewerComponent } from './components/viewer/viewer.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { PageComponent } from './components/page/page.component';
 import { CardModule } from 'primeng/card';
-import { DefaultComponent } from './components/workflows/default/default.component';
-import { MultiDocComponent } from './components/workflows/multi-doc/multi-doc.component';
-import { DisplayComponent } from './components/workflows/display/display.component';
-import { ViewerToolbarComponent } from './components/viewer-toolbar/viewer-toolbar.component';
-import { SplitComponent } from './components/workflows/split/split.component';
-import { PageSplitComponent } from './components/workflows/split/page/page.component';
 import { DragDropModule } from 'primeng/dragdrop';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TabViewModule } from 'primeng/tabview';
+import { DocumentComponent } from './components/document/document.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { PageComponent } from './components/page/page.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ViewerToolbarComponent } from './components/viewer-toolbar/viewer-toolbar.component';
+import { ViewerComponent } from './components/viewer/viewer.component';
+import { DefaultComponent } from './components/workflows/default/default.component';
+import { DisplayComponent } from './components/workflows/display/display.component';
+import { MultiDocComponent } from './components/workflows/multi-doc/multi-doc.component';
+import { PageSplitComponent } from './components/workflows/split/page/page.component';
+import { SplitComponent } from './components/workflows/split/split.component';
+import { DocumentEditorService } from './shared/services/document-editor.service';
 
 @NgModule({
   declarations: [
@@ -35,7 +33,7 @@ import { DragDropModule } from 'primeng/dragdrop';
     ViewerToolbarComponent,
   ],
   imports: [CommonModule, TabViewModule, ButtonModule, ProgressBarModule, CardModule, DragDropModule],
-  providers: [DocumentEditorService],
+  providers: [DocumentEditorService, PathLocationStrategy],
   exports: [EditorComponent],
 })
 export class NtsDocumentEditorModule {}
