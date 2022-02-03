bb33842a2811b817c8c89c3fbdc0aaa1d483c8c6
Warren Dugan <warrendugan@GMAIL.COM>
2022-02-02 21:08:23 -0800

    fix runtime error

--- libs/schematics/src/ng-generate/utils/project.ts
+++ libs/schematics/src/ng-generate/utils/project.ts
@@ -9,8 +9,8 @@ export function getProjectPath(host: Tree, options: { project?: string | undefin
 
     const project = (workspace.projects as any)[options.project];
 
-    if (project.root.substr(-1) === '/') {
-      project.root = project.root.substr(0, project.root.length - 1);
+    if (project.root?.substr(-1) === '/') {
+      project.root = project.root?.substr(0, project.root.length - 1);
     }
 
     if (options.path === undefined) {
