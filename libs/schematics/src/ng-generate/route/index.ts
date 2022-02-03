a4e9b2468858c84ad33ac3ddf2b3398df31a37b8
Warren Dugan <warrendugan@GMAIL.COM>
2022-02-02 20:55:59 -0800

    update route gen

--- libs/schematics/src/ng-generate/route/index.ts
+++ libs/schematics/src/ng-generate/route/index.ts
@@ -10,35 +10,23 @@ import {
   Tree,
   url,
 } from '@angular-devkit/schematics';
-import { getProjectFromWorkspace } from '@angular/cdk/schematics';
-import { getWorkspace } from '@schematics/angular/utility/workspace';
 import { getProjectPath, parseName, stringUtils } from '../utils';
-import { dasherize } from '../utils/string';
 
-function buildSelector(options: any, projectPrefix: string) {
-  let selector = dasherize(options.name);
-  if (options.prefix) {
-    selector = `${options.prefix}-${selector}`;
-  } else if (options.prefix === undefined && projectPrefix) {
-    selector = `${projectPrefix}-${selector}`;
-  }
-
-  return selector;
+interface Options {
+  name: string;
+  path?: string;
 }
 
-function addFiles(options: any) {
+function addFiles(options: Options) {
   return async (host: Tree) => {
-    const workspace = await getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace);
-    options.path = getProjectPath(host, options);
+    options.path = await getProjectPath(host, options);
 
     const parsedPath = parseName(options);
 
-    (parsedPath as any).path = parsedPath.path.replace(`${options.dirName}`, `${parsedPath.name}/`);
+    (parsedPath as any).path = parsedPath.path;
 
     options.name = parsedPath.name;
     options.path = parsedPath.path;
-    options.selector = options.selector || buildSelector(options, project.prefix || '');
 
     const templateSource = apply(url('./files'), [
       template({
@@ -48,38 +36,12 @@ function addFiles(options: any) {
       move(parsedPath.path),
     ]);
 
-    return branchAndMerge(chain([mergeWith(templateSource)]));
+    return chain([mergeWith(templateSource)]);
   };
 }
 
 export default function (options: any): Rule {
   return (host: Tree, context: SchematicContext) => {
-    return chain([addFiles(options)])(host, context);
+    return chain([branchAndMerge(addFiles(options))])(host, context);
   };
 }
-
-// export default function (options: any): Rule {
-//   return async (host: Tree, context: SchematicContext) => {
-//     const workspace = await getWorkspace(host);
-//     const project = getProjectFromWorkspace(workspace, Object.keys(workspace['projects'])[0]);
-//     options.path = getProjectPath(host, options);
-
-//     const parsedPath = parseName(options);
-
-//     (parsedPath as any).path = parsedPath.path.replace(`${options.dirName}`, `${parsedPath.name}/`);
-
-//     options.name = parsedPath.name;
-//     options.path = parsedPath.path;
-//     options.selector = options.selector || buildSelector(options, project.prefix || '');
-
-//     const templateSource = apply(url('./files'), [
-//       template({
-//         ...stringUtils,
-//         ...(options as object),
-//       } as any),
-//       move(parsedPath.path),
-//     ]);
-
-//     return chain([branchAndMerge(chain([mergeWith(templateSource)]))])(host, context);
-//   };
-// }
