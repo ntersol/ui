b1739f533086a46a104920e9225c2b91760163bd
Warren Dugan <warrendugan@GMAIL.COM>
2022-02-08 20:16:29 -0800

    schematics - update module imports

--- libs/schematics/src/ng-add/index.ts
+++ libs/schematics/src/ng-add/index.ts
@@ -1,16 +1,8 @@
-import { chain, noop, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { chain, noop, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import {
-  addModuleImportToRootModule,
-  getProjectFromWorkspace,
-  getProjectMainFile,
-  insertImport,
-} from '@angular/cdk/schematics';
-import { InsertChange } from '@schematics/angular/utility/change';
+import { addModuleImportToRootModule, getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
-import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
-import * as ts from 'typescript';
 import { Schema } from './schema';
 
 function addPackageJsonDependencies(options: Schema): Rule {
@@ -25,10 +17,12 @@ function addPackageJsonDependencies(options: Schema): Rule {
       });
     }
 
-    dependencies.forEach((dependency) => {
-      addPackageJsonDependency(host, dependency);
-      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
-    });
+    if (dependencies.length) {
+      dependencies.forEach((dependency) => {
+        addPackageJsonDependency(host, dependency);
+        context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+      });
+    }
 
     return host;
   };
@@ -43,35 +37,13 @@ function installPackageJsonDependencies(): Rule {
   };
 }
 
-function getTsSourceFile(host: Tree, path: string): ts.SourceFile {
-  const buffer = host.read(path);
-  if (!buffer) {
-    throw new SchematicsException(`Could not read file (${path}).`);
-  }
-  const content = buffer.toString();
-  const source = ts.createSourceFile(path, content, ts.ScriptTarget.Latest, true);
-
-  return source;
-}
-
 function injectImports(options: Schema) {
   return async (host: Tree, _context: SchematicContext) => {
-    if (!options.documentEditor) {
-      return;
-    }
-
     const workspace = await getWorkspace(host);
     const project = getProjectFromWorkspace(workspace);
-    const modulePath = getAppModulePath(host, getProjectMainFile(project));
-    const moduleSource = getTsSourceFile(host, modulePath);
 
     if (options.documentEditor) {
-      const docsChange = insertImport(moduleSource, modulePath, 'NtsDocumentEditorModule', '@ntersol/document-editor');
-      if (docsChange) {
-        const recorder = host.beginUpdate(modulePath);
-        recorder.insertLeft((docsChange as InsertChange).pos, (docsChange as InsertChange).toAdd);
-        host.commitUpdate(recorder);
-      }
+      addModuleImportToRootModule(host, 'NtsDocumentEditorModule', '@ntersol/document-editor', project);
     }
   };
 }
@@ -87,27 +59,6 @@ function setSchematicsAsDefault(): Rule {
   };
 }
 
-function addModuleToImports(options: Schema) {
-  return async (host: Tree, context: SchematicContext) => {
-    const workspace = await getWorkspace(host);
-    const project = getProjectFromWorkspace(workspace);
-
-    let importDocumentEditor = '';
-
-    if (options.documentEditor) {
-      importDocumentEditor = `NtsDocumentEditorModule`;
-    }
-
-    if (importDocumentEditor) {
-      addModuleImportToRootModule(host, importDocumentEditor, project.sourceRoot || '', project);
-    }
-
-    if (options.documentEditor) {
-      context.logger.log('info', `🦄 NtersolDocumentEditor is imported`);
-    }
-  };
-}
-
 function log(): Rule {
   return (host: Tree, context: SchematicContext) => {
     context.logger.log('info', `👏 Create your first route by running: ng g route --name home`);
@@ -120,7 +71,6 @@ export default function ngAdd(options: Schema): Rule {
   return chain([
     options && options.skipPackageJson ? noop() : addPackageJsonDependencies(options),
     options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
-    addModuleToImports(options),
     injectImports(options),
     setSchematicsAsDefault(),
     log(),
