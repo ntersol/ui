dfb4f60433ab439392ed502ba6d42918aebf15bd
Warren Dugan <warrendugan@GMAIL.COM>
2022-02-08 19:02:18 -0800

    schematics - update site module import with ts path alias

--- libs/schematics/src/ng-generate/route/files/__name@dasherize__.module.ts
+++ libs/schematics/src/ng-generate/route/files/__name@dasherize__.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { <%= classify(name) %>Component } from './<%= dasherize(name)%>.component';
-import { SiteModule } from '../../site.module';
+import { SiteModule } from '$site';
 import { <%= classify(name)%>Routing } from './<%= dasherize(name)%>-routing.module';
 import { <%= classify(name) %>ApiService, <%= classify(name) %>UiService } from './shared/stores';
 
