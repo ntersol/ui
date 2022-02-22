acda3c420c3cebdd7bc2c5e2d18e50c8e9393670
Warren Dugan <warrendugan@GMAIL.COM>
2022-02-21 17:18:20 -0800

    adds title to pwa gen page

--- apps/showcase/src/app/routes/schematics/pwa/pwa-routing.module.ts
+++ apps/showcase/src/app/routes/schematics/pwa/pwa-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PwaComponent } from './pwa.component';
 
-const routes: Routes = [{ path: '', component: PwaComponent }];
+const routes: Routes = [{ path: '', component: PwaComponent, data: { title: 'PWA Generation' } }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PwaRoutingModule { }
+export class PwaRoutingModule {}
