dfc20b956670d1b3885888bfb0d3a9c0ee69c33f
Warren Dugan <warrendugan@GMAIL.COM>
2022-03-10 15:05:11 -0800

    add base href after scully as plugin

--- scully.showcase.config.ts
+++ scully.showcase.config.ts
@@ -1,12 +1,16 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
 import '@scullyio/scully-plugin-puppeteer';
 import './scully/plugins/plugin';
+
+const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
+setPluginConfig(baseHrefRewrite, { href: 'https://ntersol.github.io/ui/' });
 
 export const config: ScullyConfig = {
   projectRoot: './apps/showcase/src',
   projectName: 'showcase',
   outDir: './dist/static',
-  defaultPostRenderers: [],
+  defaultPostRenderers: defaultPostRenderers,
   routes: {
     '/assets': {
       type: 'skip',
