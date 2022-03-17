cbb108a4a90dc2ac807eaeb9c082b6a0b1c79ba6
Warren Dugan <warrendugan@gmail.com>
2022-03-17 09:08:45 -0700

    add max render threads to showcase scully config

--- scully.showcase.config.ts
+++ scully.showcase.config.ts
@@ -1,6 +1,7 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
 import '@scullyio/scully-plugin-puppeteer';
+import { cpus } from 'os';
 import './scully/plugins/plugin';
 
 const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
@@ -11,6 +12,7 @@ export const config: ScullyConfig = {
   projectName: 'showcase',
   outDir: './dist/static',
   defaultPostRenderers: defaultPostRenderers,
+  maxRenderThreads: cpus().length * 4,
   routes: {
     '/assets': {
       type: 'skip',
