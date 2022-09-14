e27e2f99fda54dd0b7a7682c8505b83ee9461183
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2022-09-14 13:38:13 -0400

    Remove signalR from services lib

--- libs/services/src/index.ts
+++ libs/services/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/service-worker/service-worker.service';
-export * from './lib/signal-r/signal-r.service';
 export * from './lib/dom/dom.service';
 export * from './lib/storage/storage.service';
