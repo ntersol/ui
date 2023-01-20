f7ec4814fd6ab0264db8d97e582d0ccc07067075
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-01-20 10:47:07 -0500

    Add base component

--- libs/file-uploader/src/lib/file-uploader.module.ts
+++ libs/file-uploader/src/lib/file-uploader.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    FileUploaderComponent
+  ],
 })
 export class FileUploaderModule {}
