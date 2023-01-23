06b9616cfc22fc9317815473608a0cca9a75f86e
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-01-23 13:55:36 -0500

    Better error handling

--- libs/file-uploader/src/lib/file-uploader.module.ts
+++ libs/file-uploader/src/lib/file-uploader.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageModule } from 'primeng/message';
 import { NtsFileUploaderComponent } from './file-uploader/file-uploader.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MessageModule],
   declarations: [NtsFileUploaderComponent],
   exports: [NtsFileUploaderComponent],
 })
--- libs/file-uploader/src/lib/file-uploader/file-uploader.component.ts
+++ libs/file-uploader/src/lib/file-uploader/file-uploader.component.ts
@@ -12,6 +12,13 @@ export interface FilesOutput {
   fileReader: (string | ArrayBuffer | null)[] | null;
 }
 
+interface State {
+  files: File[];
+  fileSizes: number[];
+  fileTypes: string[];
+  errors: null | string[];
+}
+
 @Component({
   selector: 'nts-file-uploader',
   templateUrl: './file-uploader.component.html',
@@ -27,10 +34,23 @@ export class NtsFileUploaderComponent implements OnInit {
   /** Description that appears below the title */
   @Input() description: string | null = 'Supports JPG, PNG, GIF, and BMP with a max file size of 3MB.';
   /** Allow the user to upload multiple files */
-  @Input() multiple?: boolean | null = false;
+  @Input() multiple?: boolean | null = true;
+  /** A string array of allowed mimetypes, IE ["image/png", "image/jpeg", "application/pdf"] */
+  @Input() allowedFileTypes?: string[] | null = ['image/png', 'image/jpeg', 'application/pdf'];
+  /** The maximum number of bytes that the user is allowed to upload. In the event of multiple files, will be the sum of them all */
+  @Input() maxFileSize?: number | null = null;
+  /** The maximum number of files allowed if multiple is true */
+  @Input() maxFiles?: number | null = null;
 
   // public filesOutput: FilesOutput = {};
   public filesOutput$ = new BehaviorSubject<FilesOutput | null>(null);
+
+  public state$ = new BehaviorSubject<State>({
+    files: [],
+    fileSizes: [],
+    fileTypes: [],
+    errors: null,
+  });
 
   public fileSizes$ = new BehaviorSubject<string[] | null>(null);
 
@@ -56,6 +76,28 @@ export class NtsFileUploaderComponent implements OnInit {
       .map((_key, i) => fileList.item(i))
       .filter(notEmpty);
 
+    // Generate initial state
+    const state: State = {
+      files: files,
+      fileSizes: files.map((file) => file.size),
+      fileTypes: files.map((file) => {
+        const split = file.name.split('.');
+        return split[split.length - 1];
+      }),
+      errors: null,
+    };
+
+    const errors = this.errorCheck(state);
+    state.errors = errors;
+
+    console.log(state);
+
+    this.state$.next(state);
+
+    if (errors) {
+      return;
+    }
+
     // Hold output of different formats
     const filesOutput: FilesOutput = {
       fileList: fileList,
@@ -71,16 +113,7 @@ export class NtsFileUploaderComponent implements OnInit {
         filesOutput.fileReader?.push(reader.result);
         // When all fileReader files have finished loading,
         if (files.length === filesOutput.fileReader?.length) {
-          this.fileSizes$.next(
-            files.map((file) => {
-              if (file.size > 1000000) {
-                return String(Math.floor(file.size / 1000000)) + ' MB';
-              } else if (file.size > 1000) {
-                return String(Math.floor(file.size / 1000)) + ' KB';
-              }
-              return String(Math.floor(file.size)) + ' Bytes';
-            }),
-          );
+          this.fileSizes$.next(files.map((file) => this.formatFileSize(file.size)));
           this.filesOutput$.next(filesOutput); // Push the fileoutput to the observable
           this.filesOutput.emit(filesOutput); // Send files to parent
           console.log(filesOutput);
@@ -88,6 +121,85 @@ export class NtsFileUploaderComponent implements OnInit {
       };
       reader.readAsDataURL(file);
     });
+  }
+
+  /**
+   * Format a filesize into a human readable label
+   * @param bytes
+   * @returns
+   */
+  private formatFileSize(bytes: number) {
+    if (bytes > 1000000) {
+      return String(Math.floor(bytes / 1000000)) + ' MB';
+    } else if (bytes > 1000) {
+      return String(Math.floor(bytes / 1000)) + ' KB';
+    }
+    return String(Math.floor(bytes)) + ' Bytes';
+  }
+
+  /**
+   * Check errors and return error messages
+   * @param state
+   * @returns
+   */
+  private errorCheck(state: State) {
+    const errors: string[] = [];
+    const totalFileSize = state.fileSizes.reduce((a, b) => a + b, 0);
+    const isValid = this.allowedFileTypes?.length
+      ? state.files.every((file) => this.allowedFileTypes?.includes(file.type))
+      : true;
+
+    // Max filesize
+    if (this.maxFileSize && this.maxFileSize < totalFileSize) {
+      this.multiple && state.fileSizes?.length > 1
+        ? errors.push(
+            `The files you are trying to upload exceeds the maximum allowed filesize. The maximum allowed filesize is ${this.formatFileSize(
+              this.maxFileSize,
+            )} but the files you attempted to upload was ${this.formatFileSize(
+              totalFileSize,
+            )}. Please upload files with a smaller size.`,
+          )
+        : errors.push(
+            `The file you are trying to upload exceeds the maximum allowed filesize. The maximum allowed filesize is ${this.formatFileSize(
+              this.maxFileSize,
+            )} but the file you attempted to upload was ${this.formatFileSize(
+              totalFileSize,
+            )}. Please upload a file with a smaller size.`,
+          );
+    }
+
+    // Max files
+    if (this.maxFiles && this.maxFiles < state.fileSizes.length) {
+      errors.push(
+        `You have exceeded the maximum number of files that can be uploaded at once. The maximum allowed number of files is ${
+          this.maxFiles
+        }, but you attempted to upload ${state.fileSizes.length + 1}. Please reduce the number of files and try again.`,
+      );
+    }
+
+    // Approved filetypes
+    if (this.allowedFileTypes && !isValid) {
+      const allowedTypes = this.allowedFileTypes
+        .map((type) => {
+          const split = type.split('/');
+          return split[1].toUpperCase();
+        })
+        .reduce((prev, curr, index) => {
+          // Check if this is the last item in the array
+          if (this.allowedFileTypes && index === this.allowedFileTypes?.length - 1) {
+            return prev + ' and ' + curr;
+          } else if (index === 0) {
+            return curr;
+          } else {
+            return prev + ', ' + curr;
+          }
+        });
+      errors.push(
+        `The files you are trying to upload are of the wrong type. You can only upload ${allowedTypes}. Please select the correct file type and try again."`,
+      );
+    }
+
+    return errors.length ? errors : null;
   }
 
   /**
