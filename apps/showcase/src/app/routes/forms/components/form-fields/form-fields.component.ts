371710aad7ac6409757290d64e697687a3eb2a39
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-03-21 10:39:11 -0400

    Add documentation

--- apps/showcase/src/app/routes/forms/components/form-fields/form-fields.component.ts
+++ apps/showcase/src/app/routes/forms/components/form-fields/form-fields.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { requiredValidator, numberIsGreaterThanValidator } from 'libs/forms/src/lib/validators';
+import { HighlightService } from '../../../../shared/services/highlight.service';
+import { FileUploadService } from '../../../components/routes/file-uploader/file-uploader.service';
 
 @Component({
   selector: 'nts-form-fields-demo',
   templateUrl: './form-fields.component.html',
   styleUrls: ['./form-fields.component.scss'],
 })
-export class FormFieldsDemoComponent implements OnInit {
+export class FormFieldsDemoComponent implements OnInit, AfterViewInit {
   public form = this.fb.group({
-    sink: [null, requiredValidator],
-    text: [null, requiredValidator],
-    textField: [null, requiredValidator],
-    number: [null, numberIsGreaterThanValidator(5)],
+    sink: [null],
+    text: [null],
+    textField: [null],
+    number: [null],
+    currency: [null],
     date: [''],
     dropdown: [''],
     email: [''],
@@ -30,7 +32,31 @@ export class FormFieldsDemoComponent implements OnInit {
     { label: 'Arizona', value: 'AZ' },
   ];
 
-  constructor(private fb: FormBuilder) {}
+  public sink = this.highlight.htmlEncode(`
+  <nts-form-field-text
+      label="Kitchen Sink Example"
+      placeholder="Placeholder"
+      hint="This is a hint"
+      suffix="suffix"
+      prefix="prefix"
+      automationId="automationID"
+      [control]="form.get('sink')">
+  </nts-form-field-text>
+    `);
+
+  public text = this.highlight.htmlEncode(`
+  <nts-form-field-text
+    label="Text Example"
+    [control]="form.get('text')">
+  </nts-form-field-text>`);
+
+  public number = this.highlight.htmlEncode(`
+  <nts-form-field-number
+    label="Number Example"
+    [control]="form.get('number')">
+  </nts-form-field-number>`);
+
+  constructor(private fb: FormBuilder, private highlight: HighlightService, public svc: FileUploadService) {}
 
   ngOnInit(): void {
     this.form.valueChanges.subscribe(console.log);
@@ -39,6 +65,9 @@ export class FormFieldsDemoComponent implements OnInit {
   public submit() {
     this.form.markAllAsTouched();
     this.form.patchValue(this.form.value);
-    console.log(this.form.invalid);
+  }
+
+  ngAfterViewInit() {
+    this.highlight.highlightAll();
   }
 }
